5f90977632dba5e9068cd7dda2224d912733d1a6
Juventz ARISTIL MATHURIN <jaristil@made-f0Br6s18.clusters.42paris.fr>
2023-09-27 18:03:49 +0200

    add heredoc and chevron

---
+++ src/exec/exec_redir.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_redir.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaristil <jaristil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/27 15:12:32 by jaristil          #+#    #+#             */
+/*   Updated: 2023/09/27 18:01:52 by jaristil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+void	do_redir(t_data *data, t_token *token, int type)
+{
+	ft_close_fd(data->fd_out);
+	if (type == CHEVRON)
+		data ->fd_out = open(token->str, O_CREAT, O_WRONLY);
+		// verifier les flags pour open
+	else
+		data->fd_in = open(token->str, O_CREAT, O_WRONLY);
+	if (data->fd_out < 0)
+	{
+		ft_putstr_fd(token->str, STDERR);
+		ft_putendl_fd(ERR_OPEN, STDERR);
+		data->result = 1;
+		data->err_redir = 1;
+		return ;
+	}
+	dup2(data->fd_out, STDOUT);
+}
+
+void	redir_chev(t_data *data, t_token *token)
+{
+	ft_close_fd(data->fd_in);
+	// in pipex it make mistake but normally fixed and ensure that
+	// the old open file is well closed
+	data->fd_in = open(token->str, O_RDONLY);
+	// verifier les flags
+	if (data->fd_in < 0)
+	{
+		ft_putstr_fd(token->str, STDERR);
+		ft_putendl_fd(ERR_OPEN, STDERR);
+		data->result = FAILURE;
+		data->err_redir = FAILURE;
+		return ;
+	}
+	dup2(data->fd_in, STDIN);
+}
+
+void	open_heredoc(t_data *data)
+{
+	data->fd_in = open(".heredoc", O_RDONLY);
+	// verifiez les flags
+	if (data->fd_in < 0)
+	{
+		ft_putstr_fd(ERR_OPEN, STDERR);
+		data->result = FAILURE;
+		data->err_redir = FAILURE;
+		return ;
+	}
+	unlike(".heredoc");
+	dup2(data->fd_in, STDIN);
+}
+
+// create pipe from in to take all the input write in the << data->pipefd[0]
+// and  give the out of pipe to write data->pipefd[1]
+void	redir_heredoc(t_data *data, t_token *token)
+{
+	char	*hd_file;
+	int		fd;
+
+	unlike(".heredoc");
+	fd = open(".heredoc", O_WRONLY | O_CREAT);
+	// verifier flags
+	if (fd < 0 || !(token->str) || ft_strlen(token->str) == 0)
+	{
+		ft_putstr_fd(token->str, STDERR);
+		ft_putendl_fd(ERR_OPEN, STDERR);
+		data->err_redir = FAILURE;
+		data->result = FAILURE;
+		return ;
+	}
+	// hd_file = // ce que ju recup sous forme de *str(token->str);
+	if (!hd_file)
+		ft_putstr_fd("", fd);
+		// it write an empty stirng inside the file .heredoc
+		// so jules has to return NULL if he can't find occurence
+	else
+		ft_putstr_fd(hd_file, fd);
+	free(hd_file);
+	close(fd);
+	open_heredoc(data);
+}
--- src/exec/start_exec.c
+++ src/exec/start_exec.c
@@ -6,62 +6,13 @@
 /*   By: jaristil <jaristil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 16:26:36 by jaristil          #+#    #+#             */
-/*   Updated: 2023/09/26 18:36:09 by jaristil         ###   ########.fr       */
+/*   Updated: 2023/09/27 18:02:33 by jaristil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int	do_pipe(t_data *data)
-{
-	pid_t	pid;
-	int		pipefd[2];
-
-	if (pipe(pipefd) < 0)
-		return (perror("pipe"), FAILURE);
-	pid = fork();
-	if (pid == 0)
-	{
-		ft_close_fd(pipefd[1]);
-		dup2(pipefd[0], STDIN);
-		data->pipe_in = pipefd[0];
-		data->pid = -1;
-		data->parent = 0;
-		data->err_redir = 0;
-		return (CHILD);
-	}
-	else
-	{
-		ft_close_fd(pipefd[0]);
-		dup2(pipefd[1], STDOUT);
-		data->pipe_out = pipefd[1];
-		data->pid = pid;
-		data->end = 0;
-		return (PARENT);
-	}
-}
-
-void	do_redir(t_data *data, t_token *token, int type)
-{
-	ft_close_fd(data->fd_out);
-	if (type == CHEVRON)
-		data ->fd_out = open(token->str, O_CREAT, O_WRONLY);
-		// verifier les flags pour open
-	else
-		data->fd_in = open(token->str, O_CREAT, O_WRONLY);
-	if (data->fd_out < 0)
-	{
-		ft_putstr_fd(token->str, STDERR);
-		ft_putendl_fd(ERR_OPEN, STDERR);
-		data->result = 1;
-		data->err_redir = 1;
-		return ;
-	}
-	dup2(data->fd_out, STDOUT);
-}
-
-
-void	redir_exec(t_data *data, t_token *token)
+void	exec_redir(t_data *data, t_token *token)
 {
 	int		pipe;
 	t_token	*next_tok;
@@ -70,23 +21,25 @@ void	redir_exec(t_data *data, t_token *token)
 	pipe = 0;
 	next_tok = get_next_token(token, 0);
 	prev_tok = get_prev_token(token, 0);
-	if (is_type(prev_tok, CHEVRON))
+	if (check_token(prev_tok, CHEVRON))
 		do_redir(data, token, CHEVRON);
-	else if (is_type(prev_tok, DOUBLE_CHEVRON))
+	else if (check_token(prev_tok, DOUBLE_CHEVRON))
 		do_redir(data, token, DOUBLE_CHEVRON);
-	else if (is_type(prev_tok, PIPE))
+	else if (check_token(prev_tok, PIPE))
 		pipe = do_pipe(data);
-	//else if (is_type(prev_tok, OPEN_CHEVRON))
-	//	is_open_chev(data, token);
-	//else if (is_type(prev_tok, HERE_DOC))
-	//	is_here_doc(data, token);
-	if (next_tok && is_type(next_tok, END) == 0 && pipe != 1)
-		redir_exec(data, next_tok->next);
-	if ((is_type(prev_tok, END) || is_type(prev_tok, PIPE) || !prev_tok)
+	else if (check_token(prev_tok, OPEN_CHEVRON))
+		redir_chev(data, token);
+	else if (check_token(prev_tok, HERE_DOC))
+		redir_heredoc(data, token);
+	if (next_tok && check_token(next_tok, END) == 0 && pipe != 1)
+		exec_redir(data, next_tok->next);
+	if ((check_token(prev_tok, END) == SUCCESS
+			|| check_token(prev_tok, PIPE) == SUCCESS || !prev_tok)
 		&& data->err_redir == 0 && pipe != 1)
 		exec_command(data, token);
 }
 
+// check les appels systeme
 void	launch_minishell(t_data *data)
 {
 	int		status;
@@ -96,7 +49,7 @@ void	launch_minishell(t_data *data)
 		data->parent = 1;
 		data->exec = 1;
 		data->end = 1;
-		redir_exec(data, data->token);
+		exec_redir(data, data->token);
 		ft_close_all_fd(data);
 		reset_to_initial_fd(data);
 		dup2(data->in, STDIN);
