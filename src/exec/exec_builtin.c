ddba5cee47731f36ed6cc1ddc572ff96e2eb0fda
Jules DUVAL <juduval@made-f0Br6s19.clusters.42paris.fr>
2023-10-24 20:34:56 +0200

    deleted some comments ready for next day

--- src/exec/exec_builtin.c
+++ src/exec/exec_builtin.c
@@ -6,7 +6,7 @@
 /*   By: juduval <juduval@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 13:40:34 by jaristil          #+#    #+#             */
-/*   Updated: 2023/10/24 20:27:28 by juduval          ###   ########.fr       */
+/*   Updated: 2023/10/24 20:34:20 by juduval          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,21 +26,8 @@ int	is_builtin(char *cmd)
 		return (1);
 	else if (ft_strcmp("env", cmd) == 0)
 		return (1);
-	// else if (ft_strcmp("exit", cmd) == 0)
-	// 	return (1);
 	return (0);
 }
-
-// static int	exit_function(t_data *data, char **cmd)
-// {
-// 	make_exit(data, cmd);
-// 	free_and_close_data(data, 59);
-// 	free_env(&data->env);
-// 	free(data->pids);
-// 	free_tab(cmd);
-// 	exit (data->result);
-// 	return (0);
-// }
 
 int	exec_builtin(t_data *data, char **cmd, t_token *token)
 {
@@ -68,7 +55,5 @@ int	exec_builtin(t_data *data, char **cmd, t_token *token)
 	}
 	else if (ft_strcmp("env", cmd[0]) == 0)
 		result = make_env(data, data->env);
-	// else if (ft_strcmp("exit", cmd[0]) == 0)
-	// 	result = exit_function(data, cmd);
 	return (result);
 }
